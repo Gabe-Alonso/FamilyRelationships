555b7dc79a2dcf7cecb0f994971fa3e720f84175
Tyler Hsia <TylerHsia@gmail.com>
2020-12-11 09:58:50 -0800

    added questions for user

--- FamilyRelationships.java
+++ FamilyRelationships.java
@@ -1,12 +1,21 @@
 import java.util.*;
 
+import org.graalvm.compiler.phases.common.ConditionalEliminationUtil.InputFilter;
+
 public class FamilyRelationships {
 
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
         System.out.println("This program will show you all of the ancestors and descendant of a person");
+
+        /*
         System.out.println("Where is the input file?");
-        
+        String inputFile = input.nextLine();
         System.out.println("Person's name ('quit' to end)?");
+        String personName = input.nextLine();
+        */
+        String inputFile = "tudor.dat";
+        String personName = "Henry VII";
     }
 
     public static void ancestors(){

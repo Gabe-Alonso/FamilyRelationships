4f0d8099dcd70dc12906d0dd6aaee85ca5568876
Tyler Hsia <TylerHsia@gmail.com>
2020-12-11 09:44:35 -0800

    added ancestors and descendents methods

--- FamilyRelationships.java
+++ FamilyRelationships.java
@@ -3,4 +3,12 @@ public class FamilyRelationships {
     public static void main(String[] args){
         
     }
+
+    public static void ancestors(){
+
+    }
+
+    public static void descendents(){
+        
+    }
 }

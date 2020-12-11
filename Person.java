fc34d540abde6a3b19be7055ce8692145e93a88f
Tyler Hsia <TylerHsia@gmail.com>
2020-12-11 09:39:06 -0800

    Person is hopefully working

--- Person.java
+++ Person.java
@@ -1,23 +1,26 @@
-
+import java.util.*;
 //tyler 
 public class Person {
     private String personName;
-    Person mother;
-    Person father;
-    
+    private Person mother;
+    private Person father;
+    private ArrayList<Person> kids;
+
+
     public Person(String name){
         personName = name;
+        kids = new ArrayList<Person>();
     }
-	public void setMother(Person mother) {
-
+	public void setMother(Person motherIn) {
+        mother = motherIn;
 	}
 
 	public void addKid(Person person) {
-
+        kids.add(person);
 	}
 
-	public void setFather(Person father) {
-
+	public void setFather(Person fatherIn) {
+        father = fatherIn;
 	}
     
 

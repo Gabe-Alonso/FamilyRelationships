b2a1678d59e78543c29e9284477ef856aa8801bc
Tyler Hsia <TylerHsia@gmail.com>
2020-12-11 09:36:55 -0800

    Initial person class

---
+++ Person.java
@@ -0,0 +1,24 @@
+
+//tyler 
+public class Person {
+    private String personName;
+    Person mother;
+    Person father;
+    
+    public Person(String name){
+        personName = name;
+    }
+	public void setMother(Person mother) {
+
+	}
+
+	public void addKid(Person person) {
+
+	}
+
+	public void setFather(Person father) {
+
+	}
+    
+
+}

cf2d51f513d18c8937a66076a3d0f7047a66a6c7
TJ5__ <tejesh.anand@hotmail.com>
2020-12-11 09:59:08 -0800

    main method initial commit

--- FamilyRelationships.java
+++ FamilyRelationships.java
@@ -1,6 +1,18 @@
+import java.util.*;
+import java.io.*;
 public class FamilyRelationships {
 
     public static void main(String[] args){
+        try {
+            Scanner input = new Scanner(new File("tudor.dat"));
+            FamilyInfo parser = new FamilyInfo();
+            parser.read(input);
+            //The familyinfo object now has a field that is an arraylist of all Person objects
+            //TODO: implement user experience, call ancestors or descendants
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
         
     }
 

4947709602f9c09180b9423fbc0f9c64ab529037
Tyler Hsia <TylerHsia@gmail.com>
2020-12-11 09:56:39 -0800

    added getters for mother and father

--- Person.java
+++ Person.java
@@ -31,6 +31,13 @@ public class Person {
     public String getName() {
         return personName;
     }
+
+    public Person getMother(){
+        return mother;
+    }
+    public Person getFather(){
+        return father;
+    }
     
 
 }

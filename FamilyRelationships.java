f24bd2f7f12dffc697d7bbe7480d2298796a7dfb
Tyler Hsia <TylerHsia@gmail.com>
2020-12-11 10:10:10 -0800

    added coments to person.java and who did what

--- FamilyRelationships.java
+++ FamilyRelationships.java
@@ -3,6 +3,7 @@ import java.io.*;
 public class FamilyRelationships {
 
     public static void main(String[] args){
+        //tj
         try {
             Scanner input = new Scanner(new File("tudor.dat"));
             FamilyInfo parser = new FamilyInfo();
@@ -13,7 +14,7 @@ public class FamilyRelationships {
             e.printStackTrace();
         }
         
-
+        //tyler
         /*
         System.out.println("Where is the input file?");
         String inputFile = input.nextLine();
@@ -24,11 +25,13 @@ public class FamilyRelationships {
         String personName = "Henry VII";
     }
 
+    //yash
     public static void ancestors(Person person, int level){
         Person mother = person.getMother();
         Person father = person.getFather();
     }
 
+    //gabe
     public static void descendants(Person p, int level){
         if(!(p.getMother().getName().equals("unknown"))){
             for(int i = 0; i <= level; i++){
--- Person.java
+++ Person.java
@@ -11,30 +11,32 @@ public class Person {
     private Person father;
     private ArrayList<Person> kids;
 
-
+    //constructs a person with the name given
     public Person(String name){
         personName = name;
         kids = new ArrayList<Person>();
     }
+    //sets the person's mother to the Person inputted
 	public void setMother(Person motherIn) {
         mother = motherIn;
 	}
-
+    //Adds child inputted to arraylist of children 
 	public void addKid(Person person) {
         kids.add(person);
 	}
-
+    //sets the person's father to the Person inputted
 	public void setFather(Person fatherIn) {
         father = fatherIn;
     }
-    
+    //returns name of given person 
     public String getName() {
         return personName;
     }
-
+    //returns mother of given person 
     public Person getMother(){
         return mother;
     }
+    //returns father of given person 
     public Person getFather(){
         return father;
     }

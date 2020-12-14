0427475015b28edaff7006f7a595be5b186f39d5
Gabe-Alonso <67928782+Gabe-Alonso@users.noreply.github.com>
2020-12-14 14:10:32 -0800

    Update FamilyRelationships.java

--- FamilyRelationships.java
+++ FamilyRelationships.java
@@ -46,24 +46,42 @@ public class FamilyRelationships {
     }
 
     //gabe
+    //Prints out the desired first person's name and begins the recursive process
     public static void ancestors(Person p){
         System.out.println("\t" + p.getName());
         ancestors(p, 1);
     }
 
+    //Prints out all of the desired person's ancestors
+    //Inhererits a mother/father and level of tabs
     public static void ancestors(Person p, int level){
+        //Recursively goes through every mother and prints them with proper spacing
+
+        //Makes sure the mother exists
         if(p.getMother() != null && !(p.getMother().getName().equals("unknown"))){
+            //Spaces out the person's name
             for(int i = 0; i <= level; i++){
                 System.out.print("\t");
             }
+            //Print's the mother's name
             System.out.println(p.getMother().getName());
+            //Recurisively sends through this mother's mother into the ancestors method with
+            //an increment on level
             ancestors(p.getMother(), level + 1);
         }
+
+        //Recursively goes through every father and prints them with proper spacing
+
+        //Makes sure the father exists
         if(p.getFather() != null && !(p.getFather().getName().equals("unknown"))){
+            //Spaces out the person's name
             for(int i = 0; i <= level; i++){
                 System.out.print("\t");
             }
+            //Print's the father's name
             System.out.println(p.getFather().getName());
+            //Recurisively sends through this father's father into the ancestors method with
+            //an increment on level
             ancestors(p.getFather(), level + 1);
         }
     }
